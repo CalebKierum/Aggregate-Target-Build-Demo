2562f937c926ef06744854d25b653e42e7e05500
Caleb Kierum <kierumcak@gmail.com>
2025-08-04 23:47:21 -0700

    Initial Commit

---
+++ BrowserApp/BrowserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserAppApp.swift
+//  BrowserApp
+//
+//  Created by Caleb Kierum on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BrowserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrowserApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrowserApp
+//
+//  Created by Caleb Kierum on 8/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
